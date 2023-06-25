743225fce052eef3cebe5640113f5eb9eca7f33d
zhengyy <zhengyy1525@qq.com>
2023-06-25 15:09:29 +0800

    basement

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'; // @ts-ignore
 import App from './app.vue';
 import { router } from '@renderer/router';
 import pinia from './stores';
-import './assets/css/index.css';
+// import './assets/css/index.css';
 import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
